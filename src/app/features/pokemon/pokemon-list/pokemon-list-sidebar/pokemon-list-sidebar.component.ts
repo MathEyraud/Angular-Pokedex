56ffdb6a64c83668ca79bce3610a7b65e7821af8
Eyraud Mathieu <eyraud-mathieu@outlook.fr>
2024-08-16 17:04:59 +0200

    feat(pokemon): enhance Pokémon selection and dropdown management
    This commit introduces several enhancements to the Pokémon selection and dropdown management within the Pokémon application, aiming to improve user interaction and visual feedback.
    
    **Loading Indicator Implementation:**
    - Added a `isLoading` indicator in `pokemon-details.component` to display a spinner during Pokémon details loading.
    - The `isLoading` indicator is activated before data loading and deactivated once the data is successfully retrieved, ensuring a responsive UI.
    
    **Sidebar List Component Optimization:**
    - Optimized the Pokémon list sidebar to automatically center on the selected Pokémon.
    - Implemented logic to load additional Pokémon into the list if the selected Pokémon is not initially visible.
    - Added a check to determine if the user navigates to the page via a direct URL or through app navigation.
    
    **Visual Styles for Selected Pokémon:**
    - Introduced CSS classes `selected-pokemon` and `not-selected-pokemon` to visually distinguish the selected Pokémon in the list.
    - Updated `pokemon-summary-tile.component.css` and `.html` to apply these styles using `ngClass` based on the selection.
    
    **Loading State Management Enhancements:**
    - Used `BehaviorSubject` in `PokemonListSidebarComponent` to track and manage the initial loading state and perform actions upon completion.
    - Refactored loading methods to better handle pagination and the display of the selected Pokémon.
    
    **Selection Detection Fix:**
    - Corrected the logic in `PokemonListSidebarComponent` to accurately handle Pokémon selection on initial load.
    
    **PokemonSummaryTileComponent Improvement:**
    - Updated `pokemon-summary-tile.component.ts` to incorporate an `isSelected` input, reflecting the selection state dynamically.
    
    **Code Cleanup and Minor Improvements:**
    - Fixed minor bugs, adjusted imports, and cleaned up comments for enhanced code clarity.
    - Improved logging in `pokemon-list.component.ts` for better tracking and diagnostics.
    
    These updates significantly improve the functionality and user experience of the Pokémon selection and display mechanisms within the application.

--- src/app/features/pokemon/pokemon-list/pokemon-list-sidebar/pokemon-list-sidebar.component.ts
+++ src/app/features/pokemon/pokemon-list/pokemon-list-sidebar/pokemon-list-sidebar.component.ts
@@ -1,5 +1,9 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { PokemonListComponent } from '../pokemon-list.component';
+import { BehaviorSubject, filter, Subject, takeUntil } from 'rxjs';
+
+const ITEMS_PER_LOAD = 25;
+const SCROLL_THRESHOLD = 100;
 
 @Component({
   selector: 'app-pokemon-list-sidebar',
@@ -11,8 +15,24 @@ export class PokemonListSidebarComponent extends PokemonListComponent implements
   // -------- //
   // ATTRIBUT //
   // -------- //
-  @ViewChild('sidebarScroll') 
-  sidebarScroll !: ElementRef;
+
+  // ViewChild permet d'obtenir une référence à l'élément HTML qui contient la liste des Pokémon
+  @ViewChild('sidebarScroll') sidebarScroll !: ElementRef;
+
+  // Utilisation d'un BehaviorSubject pour suivre l'état de chargement initial
+  private loadingState$ = new BehaviorSubject<boolean>(false);
+
+  // Utilisation d'un BehaviorSubject pour suivre l'état de chargement initial
+  private loadDataInListBar$ = new BehaviorSubject<boolean>(false);
+
+  // Variable pour éviter un scroll a chaque chargement de nouveau pokemon
+  private firstRender = false;
+
+  private destroy$ = new Subject<void>();
+
+
+
+
 
   // ----- //
   // HOOKS //
@@ -22,20 +42,67 @@ export class PokemonListSidebarComponent extends PokemonListComponent implements
 
     super.ngOnInit();
 
-    // Récupération via la page d'acceuil classique
-    this.route.params.subscribe(params => {
-
-      let id;
-
-      if (params['id'] != null) {
-        this.selectedPokemonId = params['id'];
-      }
-
-    })
+    this.initRouteParamsSubscription();       // Récupération via la page d'acceuil classique
+    this.initPokemonLoadingSubscription();    // S'abonner à la mise à jour du tableau des Pokémon pour savoir quand le premier chargement est terminé
+    this.initLoadingStateSubscription();      // Abonnement : Réagit au chargement initial complet
+    this.initLoadDataInListBarSubscription(); // Abonnement : Réagit au chargement complet de la barre latérale
   }
 
+  private initRouteParamsSubscription(): void {
+
+    // Si un ID est présent dans les paramètres d'URL, on le récupère
+    this.route.params.pipe(
+
+      takeUntil(this.destroy$),
+      filter(params => params['id'] != null)
+
+    ).subscribe(params => {
+      this.selectedPokemonId = +params['id'];
+    });
+  }
+
+  private initPokemonLoadingSubscription(): void {
+
+    this.pokemonPaginationService.pokemons$.pipe(
+
+      takeUntil(this.destroy$),
+      filter(pokemons => pokemons.length > 0)
+
+    ).subscribe(() => {
+      this.loadingState$.next(true);
+    });
+  }
+
+  private initLoadingStateSubscription(): void {
+
+    this.loadingState$.pipe(
+
+      takeUntil(this.destroy$),
+      filter(isComplete => isComplete && !this.firstRender)
+
+    ).subscribe(() => {
+      this.checkAndLoadSelectedPokemon();
+    });
+  }
+
+  private initLoadDataInListBarSubscription(): void {
+
+    this.loadDataInListBar$.pipe(
+
+      takeUntil(this.destroy$),
+      filter(isComplete => isComplete)
+
+    ).subscribe(() => {
+      this.checkAndLoadSelectedPokemon();
+    });
+  }
+
+  // Initialisation après le rendu de la vue
   ngAfterViewInit() {
-    this.sidebarScroll.nativeElement.addEventListener('scroll', this.onSidebarScroll.bind(this));
+    if (this.sidebarScroll) {
+      this.sidebarScroll.nativeElement.addEventListener('scroll', this.onSidebarScroll.bind(this));
+    }    
+    this.scrollToSelectedPokemon();
   }
 
   // Méthode de nettoyage lors de la destruction du composant
@@ -46,16 +113,98 @@ export class PokemonListSidebarComponent extends PokemonListComponent implements
     if (this.sidebarScroll) {
       this.sidebarScroll.nativeElement.removeEventListener('scroll', this.onSidebarScroll);
     }
+
+    // Nettoyer l'abonnement au BehaviorSubject
+    this.loadingState$.complete();
+    this.loadDataInListBar$.complete();
+    this.destroy$.next();
+    this.destroy$.complete();
   }
+
+
+
+
 
   // -------- //
   // LISTENER //
   // -------- //
   // Écouteur d'événement de défilement
   onSidebarScroll(): void {
+
     const element = this.sidebarScroll.nativeElement;
-    if (element.scrollHeight - element.scrollTop <= element.clientHeight + this.scrollThreshold) {
+
+    if (element.scrollHeight - element.scrollTop <= element.clientHeight + SCROLL_THRESHOLD) {
       this.loadMorePokemons();
     }
+  }
+
+
+
+
+
+  // -------- //
+  // METHODES //
+  // -------- //
+  // Ajout de la méthode dans onSelectPokemon
+  override onSelectPokemon(id: number): void {
+    super.onSelectPokemon(id);
+    this.scrollToSelectedPokemon();
+  }
+
+  // Méthode pour vérifier si le Pokémon sélectionné est dans la liste et charger si nécessaire
+  private checkAndLoadSelectedPokemon(): void {
+
+    // Vérifie si le Pokémon sélectionné est déjà dans la liste chargée
+    const selectedPokemon = this.pokemons.find(pokemon => pokemon.id === this.selectedPokemonId);
+
+    if (!selectedPokemon) {
+      this.loadPokemonBatch();
+    
+    // Centre la vue si le Pokémon est déjà chargé
+    } else {
+      this.scrollToSelectedPokemon(); 
+    }
+  }
+
+  private loadPokemonBatch(): void {
+
+    const estimatedOffset = Math.max(0, this.selectedPokemonId - ITEMS_PER_LOAD);
+
+    if (this.currentOffset < estimatedOffset) {
+      this.itemsPerLoad = estimatedOffset + ITEMS_PER_LOAD;
+    }
+
+    this.loadMorePokemons();
+
+    setTimeout(() => {
+      this.itemsPerLoad = ITEMS_PER_LOAD;
+      this.loadingState$.next(true);
+    }, 0);
+  }
+
+
+  // Méthode pour centrer la vue sur le Pokémon sélectionné
+  scrollToSelectedPokemon() {
+      
+    const sidebarElement    = this.sidebarScroll.nativeElement;
+    
+    // Trouver l'élément du Pokémon sélectionné dans la sidebar
+    const selectedElement = sidebarElement.querySelector(`#pokemon-${this.selectedPokemonId}`);
+    
+    if (selectedElement instanceof HTMLElement) {
+
+      // Calculer la position pour centrer l'élément
+      const sidebarHeight = sidebarElement.clientHeight;
+      const elementTop    = selectedElement.offsetTop;
+      const elementHeight = selectedElement.clientHeight;
+      
+      // Faire défiler pour centrer l'élément avec une animation fluide
+      sidebarElement.scrollTo({
+        top: elementTop - (sidebarHeight / 2) + (elementHeight / 2),
+        behavior: 'smooth',
+      });
+
+      this.firstRender = true;
+    };
   }
 }
